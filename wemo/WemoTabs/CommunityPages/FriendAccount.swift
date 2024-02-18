bf684ea9ae919f9bf78d17c682f575b33d7619ae
Hannah Kim <hkim24@stanford.edu>
2024-02-18 07:02:18 -0800

    front-end for community page

---
+++ wemo/WemoTabs/CommunityPages/FriendAccount.swift
@@ -0,0 +1,18 @@
+//
+//  FriendAccount.swift
+//  wemo
+//
+//  Created by Hannah Kim on 2/18/24.
+//
+
+import SwiftUI
+
+struct FriendAccount: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendAccount()
+}
