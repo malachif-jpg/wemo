a90349f5fc605aa4883bb374dd3fcf9b2b879062
Hannah Kim <hkim24@stanford.edu>
2024-02-18 08:25:53 -0800

    aura notification display

---
+++ wemo/WemoTabs/LoginView().swift
@@ -0,0 +1,18 @@
+//
+//  LoginView().swift
+//  wemo
+//
+//  Created by Hannah Kim on 2/18/24.
+//
+
+import SwiftUI
+
+struct LoginView__: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView__()
+}
