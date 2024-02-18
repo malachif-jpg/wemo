264703867e0085fd6b7fd16f855461240ebd19d0
Hannah Kim <hkim24@stanford.edu>
2024-02-18 02:11:41 -0800

    front-end for profile and all badge views

---
+++ wemo/WemoTabs/Badges/MonthMemories.swift
@@ -0,0 +1,18 @@
+//
+//  MonthMemories.swift
+//  wemo
+//
+//  Created by Hannah Kim on 2/18/24.
+//
+
+import SwiftUI
+
+struct MonthMemories: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MonthMemories()
+}
